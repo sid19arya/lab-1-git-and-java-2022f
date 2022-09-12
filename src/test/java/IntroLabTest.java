3fc5435ed4bd14216cde8b2a737991eaf86692e2
Jeffery Zhan <jefferyzhan84@gmail.com>
2022-09-12 17:43:43 -0400

    Filled out the stuff - jef

--- src/test/java/IntroLabTest.java
+++ src/test/java/IntroLabTest.java
@@ -17,7 +17,7 @@ public class IntroLabTest {
     public void tearDown() {
     }
 
-    @Test(timeout = 10)
+    @Test(timeout = 100)
     public void testExampleStudent() {
         String expected = "Hello! My name is Stu Dent! " +
                 "I like cats more than dogs! Welcome to CSC207!";
@@ -30,4 +30,10 @@ public class IntroLabTest {
      *       the test results.
      * As a reference, we've included testExampleStudent above.
      */
+    @Test(timeout = 100)
+    public void testZhanjeff() {
+        String expected = "Hello! My name is Jeffery! " + "I like cats more than dogs! lol";
+        String actual = IntroLab.zhanjeff();
+        assertEquals(expected ,actual);
+    }
 }
